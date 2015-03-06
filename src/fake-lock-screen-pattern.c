dba65cef4f25fabf3069110d9422ff13e686a0cc
HAYASHI Kentaro <kenhys@gmail.com>
2015-03-06 18:53:59 +0900

    Just show full screen window

---
+++ src/fake-lock-screen-pattern.c
@@ -0,0 +1,20 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[])
+{
+  GtkWidget *window;
+
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Fake lock screen pattern");
+
+  gtk_window_fullscreen(GTK_WINDOW(window));
+  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+  return 0;
+}
